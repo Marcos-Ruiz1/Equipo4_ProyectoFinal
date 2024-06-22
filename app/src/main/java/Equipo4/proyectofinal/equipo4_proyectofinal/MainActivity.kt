60d71a558ec158c1579880e5552f74d2ff440596
Marcos-Ruiz1 <Marcos.ZazuetaRuiz1@gmail.com>
2024-06-21 22:11:09 -0700

    add: se agrego pantalla de log in, registro de usuario y restaurar password

--- app/src/main/java/Equipo4/proyectofinal/equipo4_proyectofinal/MainActivity.kt
+++ app/src/main/java/Equipo4/proyectofinal/equipo4_proyectofinal/MainActivity.kt
@@ -1,10 +1,14 @@
 package Equipo4.proyectofinal.equipo4_proyectofinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val signUp: TextView = findViewById(R.id.signUp)
+        val logIn: Button = findViewById(R.id.logInButton)
+        val forgotPassword: TextView = findViewById(R.id.forgotPassword)
+
+        signUp.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            this.startActivity(intent)
+        }
+
+        logIn.setOnClickListener {
+
+        }
+
+        forgotPassword.setOnClickListener{
+            val intent = Intent(this, RecoverPassword::class.java)
+            this.startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/Equipo4/proyectofinal/equipo4_proyectofinal/RecoverPassword.kt
@@ -0,0 +1,20 @@
+package Equipo4.proyectofinal.equipo4_proyectofinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RecoverPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recover_password)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
